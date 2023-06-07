45ee1dc06ceb249c6e5aace568e13d38d8405308
AmirHossein Akbari <AkbariAmirhosseinx@gmail.com>
2023-06-07 21:30:58 +0330

    Security Helper Class Added

---
+++ Common/Utilities/SecurityHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Common.Utilities
+{
+    public class SecurityHelper
+    {
+        public static string HashPasswordSHA256(string password)
+        {
+            using var sha = SHA256.Create();
+
+            var passBytes = Encoding.UTF8.GetBytes(password);
+            var encodedBytes = sha.ComputeHash(passBytes);
+            return Convert.ToBase64String(encodedBytes);
+
+        }
+    }
+}
