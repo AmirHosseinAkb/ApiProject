3c5720a02f3c925aff4a83d9ad0491cfa6493f10
AmirHossein Akbari <AkbariAmirhosseinx@gmail.com>
2023-06-08 15:45:18 +0330

    Implicit Operators Added to ApiResult Class

--- WebFramework/Api/ApiResult.cs
+++ WebFramework/Api/ApiResult.cs
@@ -1,5 +1,6 @@
 ﻿using Common;
 using Common.Utilities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebFramework.Api
 {
@@ -14,9 +15,42 @@ namespace WebFramework.Api
         public string Message { get; set; }
         public ApiResultStatusCode StatusCode { get; set; }
         public bool IsSucceeded { get; set; }
+
+        #region Implicit Operators
+        public static implicit operator ApiResult(OkResult result)
+        {
+            return new ApiResult(true, ApiResultStatusCode.Success);
+        }
+
+        public static implicit operator ApiResult(BadRequestResult result)
+        {
+            return new ApiResult(false, ApiResultStatusCode.BadRequest);
+        }
+
+        public static implicit operator ApiResult(BadRequestObjectResult result)
+        {
+            var message = result.Value.ToString();
+            if (result.Value is SerializableError errors)
+            {
+                var errorMessages = errors.SelectMany(p => (string[])p.Value).Distinct();
+                message = string.Join(" | ", errorMessages);
+            }
+            return new ApiResult(false, ApiResultStatusCode.BadRequest, message);
+        }
+
+        public static implicit operator ApiResult(ContentResult result)
+        {
+            return new ApiResult(true, ApiResultStatusCode.Success, result.Content);
+        }
+
+        public static implicit operator ApiResult(NotFoundResult result)
+        {
+            return new ApiResult(false, ApiResultStatusCode.NotFound);
+        }
+        #endregion
     }
 
-    public class ApiResult<TData>:ApiResult
+    public class ApiResult<TData>:ApiResult where TData:class
     {
         public ApiResult(bool isSucceeded,ApiResultStatusCode statusCode,TData data,string message=null):base(isSucceeded, statusCode, message)
         {
@@ -24,5 +58,45 @@ namespace WebFramework.Api
         }
         public TData Data { get; set; }
 
+
+        #region Implicit Operators
+
+        public static implicit operator ApiResult<TData>(TData data)
+            => new ApiResult<TData>(true, ApiResultStatusCode.Success,data);
+    
+
+        public static implicit operator ApiResult<TData>(NotFoundResult result)
+            => new ApiResult<TData>(false, ApiResultStatusCode.NotFound,null);
+    
+
+        public static implicit operator ApiResult<TData>(NotFoundObjectResult result)
+            => new ApiResult<TData>(false, ApiResultStatusCode.NotFound,(TData)result.Value);
+
+        public static implicit operator ApiResult<TData>(OkResult result)
+
+            => new ApiResult<TData>(true, ApiResultStatusCode.Success, null);
+
+        public static implicit operator ApiResult<TData>(OkObjectResult result)
+            => new ApiResult<TData>(true, ApiResultStatusCode.Success, (TData)result.Value);
+
+        public static implicit operator ApiResult<TData>(ContentResult result)
+            => new ApiResult<TData>(true, ApiResultStatusCode.Success, null, result.Content);
+
+        public static implicit operator ApiResult<TData>(BadRequestResult result)
+            => new ApiResult<TData>(false, ApiResultStatusCode.BadRequest, null);
+
+        public static implicit operator ApiResult<TData>(BadRequestObjectResult result)
+        {
+            string? message = result.Value.ToString();
+            if (result.Value is SerializableError errors)
+            {
+                var errorMessages = errors.SelectMany(e => (string[]) e.Value).Distinct();
+                message = string.Join(" | ", errorMessages);
+            }
+
+            return new ApiResult<TData>(false, ApiResultStatusCode.BadRequest, null, message);
+        }
+
+        #endregion
     }
 }
