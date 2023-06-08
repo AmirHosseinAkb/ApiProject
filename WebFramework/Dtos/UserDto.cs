ffc0460ca790e9d6c40bf4341517e47252546a71
AmirHossein Akbari <AkbariAmirhosseinx@gmail.com>
2023-06-08 16:30:52 +0330

    Email Prop Deleted From UserDto

--- WebFramework/Dtos/UserDto.cs
+++ WebFramework/Dtos/UserDto.cs
@@ -5,7 +5,6 @@ namespace WebFramework.Dtos;
 public class UserDto
 {
     public string UserName { get; set; }
-    public string Email { get; set; }
     public string Password { get; set; }
     public string FullName { get; set; }
     public int Age { get; set; }
