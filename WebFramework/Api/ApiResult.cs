dd9c40f5ecc83af8d57fb91dd03afbf7d3add1d4
AmirHossein Akbari <AkbariAmirhosseinx@gmail.com>
2023-06-08 15:04:47 +0330

    Primary ApiResult Class Added

---
+++ WebFramework/Api/ApiResult.cs
@@ -0,0 +1,28 @@
+﻿using Common;
+using Common.Utilities;
+
+namespace WebFramework.Api
+{
+    public class ApiResult
+    {
+        public ApiResult(bool isSucceeded,ApiResultStatusCode statusCode,string message=null)
+        {
+            IsSucceeded = isSucceeded;
+            StatusCode= statusCode;
+            Message= message?? statusCode.ToDisplay();
+        }
+        public string Message { get; set; }
+        public ApiResultStatusCode StatusCode { get; set; }
+        public bool IsSucceeded { get; set; }
+    }
+
+    public class ApiResult<TData>
+    {
+        public ApiResult(TData data)
+        {
+            Data = data;
+        }
+        public TData Data { get; set; }
+
+    }
+}
