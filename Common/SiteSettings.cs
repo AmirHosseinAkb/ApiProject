edd5194f85fbf574eb0a1e0252f2b5ca0b62d31f
AmirHossein Akbari <AkbariAmirhosseinx@gmail.com>
2023-06-13 13:59:00 +0330

    SiteSettings Class Added

---
+++ Common/SiteSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace Common
+{
+    public class SiteSettings
+    {
+        public string ElmahPath { get; set; }
+        public JwtSettings JwtSettings { get; set; }
+    }
+
+    public class JwtSettings
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int ExpirationDays { get; set; }
+        public int NotBeforeMinutes { get; set; }
+        public string SecretKey { get; set; }
+    }
+}
